e6b64816a15a2fafab0fabb0f2e95831134eee56
Pasero Christian <c.pasero@student.tugraz.at>
2023-07-25 22:16:07 +0200

    fixed 2fa?

--- src/toasts.js
+++ src/toasts.js
@@ -26,4 +26,23 @@ export const toasts_config_success = {
     closeButton: "button",
     icon: true,
     rtl: false
-  }
+}
+
+export const toasts_config_info = {
+    position: "top-center",
+    timeout: 1533,
+    closeOnClick: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: {
+        iconClass: "undefined",
+        iconChildren: "",
+        iconTag: "i"
+    },
+    rtl: false
+}
