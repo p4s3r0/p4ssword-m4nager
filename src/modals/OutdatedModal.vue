9dc88e6a96f2b142cf04dfdbdd90220af72d2e70
Pasero Christian <c.pasero@student.tugraz.at>
2023-07-26 15:14:41 +0200

    versioning added with skip button

--- src/modals/OutdatedModal.vue
+++ src/modals/OutdatedModal.vue
@@ -2,12 +2,16 @@
     <div id="mainModal">
         <h1>Your Application is not up to Date!</h1>
         <p>Please close and exit the Application and reopen it multiple times. If this message will still be there after 5 refreshs, contact developer.</p>
+        <p>Current Version: {{ this.APP_VERSION }}</p>
+        <p>Newest Version: {{ this.new_app_version }}</p>
+        <button class="ripple" @click="this.$emit('closeModal')">Skip for Now!</button>
     </div>
 </template>
 
 <script>
 export default {
 name: 'outDatedModal',
+props: ["new_app_version"],
 components: {
 }
 }
@@ -24,6 +28,32 @@ components: {
     border: 1px white solid;
     padding: 10vw;
     border-radius: 5px;
+}
+
+button {
+    position: absolute;
+    width: 80vw;
+    height: 60px;
+    border-radius: 10px;
+    border: 1px solid rgb(68, 169, 224);
+    background: white;
+    color: black;
+    bottom: 60px;
+}
+
+
+.ripple {
+  background-position: center;
+  transition: background 0.3s;
+}
+.ripple:hover {
+  background: #ffffff radial-gradient(circle, transparent 1%, #545454 1%) center/15000%;
+  color: black;
+}
+.ripple:active {
+  background-color: #ffffff;
+  background-size: 100%;
+  transition: background 0.5s;
 }
 </style>
   
