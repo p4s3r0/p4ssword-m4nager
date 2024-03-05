55731e4236c11c43d56e8d828f395f50f7f931e0
Pasero Christian <pasero.christian99@gmail.com>
2024-03-05 18:27:26 +0100

    pwa

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,39 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    name: 'p4ssword-m4nager',
-    themeColor: '#0E0E0E',
+    manifestOptions: {
+      "name": "p4ssword-m4nager",
+      "short_name": "p4s-m4n",
+      "theme_color": "#4d55ba",
+      "icons": [
+        {
+          "src": "./icons/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "./icons/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "./icons/android-chrome-maskable-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "./icons/android-chrome-maskable-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+      "start_url": ".",
+      "display": "standalone",
+      "background_color": "#3e3e3e",
+      "description": "A simple Password Manager"
+    },
     msTileColor: '#0E0E0E',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
