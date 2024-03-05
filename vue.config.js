6a4d5b6f47fe924a6380ba19a1cbf9ee4d3a6a22
Pasero Christian <pasero.christian99@gmail.com>
2024-03-05 19:00:37 +0100

    tile color change?

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  pwa: {
-    name: 'p4ssword-m4nager',
-    themeColor: '#0E0E0E',
-    msTileColor: '#0E0E0E',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-  }
+  transpileDependencies: true
 })
